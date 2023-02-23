f15b441451560786601434c3ff87586f2d1d85b4
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-02-23 20:44:31 +0200

    new message

---
+++ testing.py
@@ -0,0 +1,8 @@
+def add(x: int, y: int) -> int:
+    return x + y
+def sub(x: int, y: int) -> int:
+    return x - y
+def prod(x: int, y: int) -> int:
+    return x * y
+def div(x: int, y: int) -> int:
+    return x // y
